967bac1409b5ef6cfb053ec1eaf3e113764602b1
deepika <deepikamarkam58@gmail.com>
2020-08-04 17:07:48 -0700

    first

---
+++ src/components/Dashboard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+// core components
+import NavBar from './NavBar';
+//import Footer1 from './Footer1';
+import GlobalHomePage from './GlobalHomePage.js';
+// import routes from 'routes.js';
+
+class Dashboard extends React.Component {
+  componentDidUpdate(e) {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }
+
+  render() {
+    return (
+      <>
+       <NavBar/>
+
+
+       <a class="btn btn-primary" href="/Profile" role="button">Profile</a>
+       <a class="btn btn-primary" href="/jobseeker" role="button">Jobseeker</a>
+       
+
+
+
+       <Footer/>
+      </>
+    );
+    // } else {
+    //   return <Redirect to="/login" exact></Redirect>;
+    // }
+  }
+}
+
+export default Dashboard;
